e554c460c2f7e7d5c14aad44f2fbc24f72f677d0
Jason Lieb <Jason.lieb@outlook.com>
2022-11-20 19:35:59 -0500

    set up npm and webpack with loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,13 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 }
